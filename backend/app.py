bf173431bff4bd9e5c7387a6ebd7cb200a36d13d
Ruhama Aworine <aworineruhama@gmail.com>
2026-05-17 00:57:05 +0600

    Stress-Detection - added new feature / bug fix

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask,request,jsonify,render_template
 import joblib
 import pandas as pd
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder="../frontend", static_url_path="")
+app = Flask(__name__,static_folder="../frontend",static_url_path="")
 CORS(app)
 
 model = joblib.load("backend/model.pkl")
@@ -12,22 +12,17 @@ le = joblib.load("backend/label_encoder.pkl")
 @app.route("/")
 def home():
     return app.send_static_file("index.html")
-
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
-
     df = pd.DataFrame([{
         "sleep_hours": float(data["sleep_hours"]),
         "heart_rate": float(data["heart_rate"]),
         "study_hours": float(data["study_hours"]),
         "screen_time": float(data["screen_time"])
     }])
-
     prediction = model.predict(df)
     result = le.inverse_transform(prediction)
-
     return jsonify({"stress_level": result[0]})
-
 if __name__ == "__main__":
     app.run(debug=True)
